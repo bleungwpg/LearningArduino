3291ddaf36e42ed8e8ea527a76bcee1f140c127c
Ben Leung <benjamin.leung80@gmail.com>
2018-11-13 11:56:01 +0800

    Update motorslightssounds.ino

--- lesson5 - motorslightssounds/motorslightssounds/motorslightssounds.ino
+++ lesson5 - motorslightssounds/motorslightssounds/motorslightssounds.ino
@@ -221,38 +221,89 @@ void loop()
   // ---------------------------
 
 
-  
+    // both wheels going backwards
   digitalWrite(10, HIGH);
-  digitalWrite(7, HIGH); 
-  delay(3000);
+  digitalWrite(7, HIGH);
+  delay(1000);
   
   
+  // both wheels stop
+  digitalWrite(10, LOW);
+  digitalWrite(7, LOW);
+  delay(1000);
+  
+  
+  
+  // both wheels going forward
   digitalWrite(11, HIGH);
   digitalWrite(6, HIGH);
-  delay(3000);
+  delay(1000);
   
-  digitalWrite(10, LOW);
-  digitalWrite(6, HIGH);  
-  delay(3000);
   
-  digitalWrite(10, HIGH);
-  digitalWrite(6, LOW);  
-  delay(3000);
-  
+  // both wheels stop
   digitalWrite(11, LOW);
-  digitalWrite(7, HIGH);  
-  delay(3000);
+  digitalWrite(6, LOW);
+  delay(1000);
   
+  
+  
+  // left wheel going forward
   digitalWrite(11, HIGH);
-  digitalWrite(7, LOW);  
-  delay(3000);
-
-  Serial.println ("Master Reset");
-  digitalWrite(6, LOW);  
-  digitalWrite(7, LOW);
-  digitalWrite(10, LOW);
+  delay(1000);
+  
+  
+  // left wheel stops
   digitalWrite(11, LOW);
-
+  delay(1000);
+  
+  
+  // left wheel going backward
+  digitalWrite(10, HIGH);
+  delay(1000);
+  
+  
+  // left wheel stops
+  digitalWrite(10, LOW);
+  delay(1000);
+  
+  // right wheel going backward
+  digitalWrite (7, HIGH);
+  delay(1000);
+  
+  // right wheel stops
+  digitalWrite(7, LOW);
+  delay(1000);
+  
+  
+  // right wheel going forward
+  digitalWrite (6, HIGH);
+  delay(1000);
+  
+  // right wheel stops
+  digitalWrite(6, LOW);
+  delay(1000);
+  
+  
+  // turn clockwise
+  digitalWrite(10, HIGH);
+  digitalWrite (6, HIGH);
+  delay(1000);
+  
+  // stop
+  digitalWrite(10, LOW);
+  digitalWrite(6, LOW);
+  delay(1000);
+  
+  
+  // turn anticlockwise
+  digitalWrite(11, HIGH);
+  digitalWrite (7, HIGH);
+  delay(1000);
+  
+  // stop
+  digitalWrite(11, LOW);
+  digitalWrite(7, LOW);
+  delay(1000);
 
 
 }
